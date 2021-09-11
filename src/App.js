e22361276ee3c83efa12e10d540d7ba4e6e2e1f8
Duong Dong Quan <dongquan15041@gmail.com>
2021-09-11 17:51:27 +0700

    update menu-item and dynamic-router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
-import HomePage from './pages/homepage.component.jsx';
+import HomePage from "./pages/homepage.component.jsx";
+import { Switch, Route, Link } from "react-router-dom";
 
-import './App.css';
+import "./App.css";
 
+const HatsPage = props => (
+  <div>
+    <h1>HATs PAGE</h1>
+    <Link to={`${props.match.url}/`}></Link>
+  </div>
+);
+const JacketsPage = props => (
+  <div>
+    <h1>JACKETs PAGE {props.match.params.topicId}</h1>
+  </div>
+);
+const SneakersPage = () => (
+  <div>
+    <h1>SNEAKERs PAGE</h1>
+  </div>
+);
+const WomenPage = () => (
+  <div>
+    <h1>WOMENs PAGE</h1>
+  </div>
+);
+const MensPage = () => (
+  <div>
+    <h1>MENs PAGE</h1>
+  </div>
+);
 function App() {
-  return(
+  return (
     <div>
-      <HomePage/>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+        <Route path="/jackets" component={JacketsPage} />
+        <Route path="/sneakers" component={SneakersPage} />
+        <Route path="/women" component={WomenPage} />
+        <Route path="/mens" component={MensPage} />
+      </Switch>
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -18,28 +18,33 @@ class Directory extends React.Component {
         {
           title: "HATs",
           imageUrl: hatsIntroImg,
+          linkUrl: 'hats',
           id: 1,
         },
         {
           title: "JACKETs",
           imageUrl: jacket,
+          linkUrl: 'jackets',
           id: 2,
         },
         {
           title: "SNEAKERs",
           imageUrl: sneaker,
+          linkUrl: 'sneakers',
           id: 3,
         },
         {
           title: "WOMENs",
           imageUrl: women,
           size: "large",
+          linkUrl: 'women',
           id: 4,
         },
         {
           title: "MENs",
           imageUrl: men,
           size: "large",
+          linkUrl: 'mens',
           id: 5,
         },
       ],
@@ -48,8 +53,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.section.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.section.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );
