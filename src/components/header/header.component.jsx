fe97c017e61b4ff43a1a20f2dbf540eed506dd45
Duong Dong Quan <dongquan15041@gmail.com>
2021-09-19 16:51:34 +0700

    styling collection-item

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { auth } from "../../firebase/firebase.utils";
+
+import CartDropDown from "../cart-dropdown/cart-dropdown.component";
+import CartIcon from "../cart-icon/cart-icon.component";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 
 import "./header.styles.scss";
@@ -11,6 +14,7 @@ const Header = ({ currentUser }) => (
     <Link className="logo-container" to="/">
       <Logo className="logo" />
     </Link>
+
     <div className="options">
       <Link className="option" to="/shop">
         SHOP
@@ -27,12 +31,16 @@ const Header = ({ currentUser }) => (
           SIGN IN
         </Link>
       )}
+      <Link className="option" to="/">
+        <CartIcon className="logo" />
+      </Link>
     </div>
+    <CartDropDown />
   </div>
 );
 
-const mapStateToProps=(state)=>({
-  currentUser:state.user.currentUser,
-})
+const mapStateToProps = (state) => ({
+  currentUser: state.user.currentUser,
+});
 
 export default connect(mapStateToProps)(Header);
