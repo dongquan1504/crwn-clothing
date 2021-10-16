a47ab3661a20d7b059bdb81c2f36a7032d397a2c
dongquan1504 <dongquan15041@gmail.com>
2021-10-16 19:15:15 +0700

    add redux-devtools-extension

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import { persistStore } from "redux-persist";
 import logger from "redux-logger";
 
@@ -6,11 +7,14 @@ import rootReducer from "./root-reducer";
 
 const middlewares = [];
 
-if(process.env.NODE_ENV==='development'){
-    middlewares.push(logger)
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
 }
 
-export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(...middlewares))
+);
 
 export const persistor = persistStore(store);
 
